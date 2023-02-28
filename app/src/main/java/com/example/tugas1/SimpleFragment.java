d70b93639687a6a069ad3aaf0e8a38b03aaaad7b
fachri <you@example.com>
2023-02-28 17:20:12 +0700

    fragment statis

---
+++ app/src/main/java/com/example/tugas1/SimpleFragment.java
@@ -0,0 +1,31 @@
+package com.example.tugas1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_simple,
+                container, false);
+
+
+        return rootView;
+    }
+}
